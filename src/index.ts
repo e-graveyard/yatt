6342d2983344a8cfe714258d0caeb0c93ed00137
Caian R. Ertl <hi@caian.org>
2021-07-30 03:01:18 -0300

    docs: remove types from jsdoc strings

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ const logger = new Logger({ isDev: true, label: 'yatt' })
 /**
  * Repeats a given callback function N times.
  *
- * @param {number} n - the amount of times to repeat.
- * @param {number} cb - the callback function.
+ * @param n - the amount of times to repeat.
+ * @param cb - the callback function.
  */
 const each = (n: number, cb: (v: any, i: number) => void): void =>
   Array.from(Array(n).keys()).forEach(cb)
@@ -16,7 +16,7 @@ const each = (n: number, cb: (v: any, i: number) => void): void =>
 /**
  * Calculate and outputs a multiplication table of a given value.
  *
- * @param {number} operand - the multiplication number.
+ * @param operand - the multiplication number.
  */
 function multiplicationTable (operand: number): void {
   logger.warn('-'.repeat(30))
